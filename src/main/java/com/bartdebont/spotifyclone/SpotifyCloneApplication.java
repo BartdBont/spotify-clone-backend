be06d5d53e0a98f3484acd51dc21271a3ac58216
Bart de Bont <430327@student.fontys.nl>
2020-11-04 23:07:13 +0100

    added swagger ui and configured it a little bit

--- src/main/java/com/bartdebont/spotifyclone/SpotifyCloneApplication.java
+++ src/main/java/com/bartdebont/spotifyclone/SpotifyCloneApplication.java
@@ -3,8 +3,18 @@ package com.bartdebont.spotifyclone;
 import com.bartdebont.spotifyclone.spotify.ClientCredentialsSpotify;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpotifyCloneApplication {
 
 //    @Bean
@@ -16,6 +26,29 @@ public class SpotifyCloneApplication {
         ClientCredentialsSpotify clientCredentialsSpotify = new ClientCredentialsSpotify();
         clientCredentialsSpotify.clientCredentials_Sync();
         SpringApplication.run(SpotifyCloneApplication.class, args);
+    }
+
+    @Bean
+    public Docket swaggerConfiguration() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/spotify/v1/**"))
+                .apis(RequestHandlerSelectors.basePackage("com.bartdebont"))
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "Spotify clone API",
+                "API for spotify clone",
+                "1.0",
+                "Free to use",
+                new springfox.documentation.service.Contact("Bart de Bont", "http://google.com", "bart_b@hotmail.nl"),
+                "API License",
+                "http://google.com",
+                Collections.emptyList()
+        );
     }
 
 }
---
+++ src/main/java/com/bartdebont/spotifyclone/util/Swagger2Configurer.java
@@ -0,0 +1,12 @@
+package com.bartdebont.spotifyclone.util;
+
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+public class Swagger2Configurer {
+
+
+}
