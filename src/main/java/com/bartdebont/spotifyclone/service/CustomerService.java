8a712a1998ae03920024803533bd45e495e10c46
Bart de Bont <430327@student.fontys.nl>
2020-11-05 12:47:28 +0100

    basic jwt authentication setup without implemented userdetailsService

--- src/main/java/com/bartdebont/spotifyclone/service/CustomerService.java
+++ src/main/java/com/bartdebont/spotifyclone/service/CustomerService.java
@@ -2,10 +2,13 @@ package com.bartdebont.spotifyclone.service;
 
 import com.bartdebont.spotifyclone.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomerService {
+public class CustomerService implements UserDetailsService {
 
     private final CustomerRepository customerRepository;
 //    private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -16,4 +19,8 @@ public class CustomerService {
     }
 
 
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
 }
