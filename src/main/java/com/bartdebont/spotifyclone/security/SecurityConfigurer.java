4193e8ae5ed4371c73d7feb5dd5290fab6ef9bf7
Bart de Bont <430327@student.fontys.nl>
2020-11-06 20:31:45 +0100

    Registering and authenticating works now but not finished yet

--- src/main/java/com/bartdebont/spotifyclone/security/SecurityConfigurer.java
+++ src/main/java/com/bartdebont/spotifyclone/security/SecurityConfigurer.java
@@ -4,16 +4,20 @@ import com.bartdebont.spotifyclone.filters.JwtRequestFilter;
 import com.bartdebont.spotifyclone.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Autowired
@@ -24,13 +28,13 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customerService);
+        auth.authenticationProvider(authProvider());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .authorizeRequests().antMatchers("/spotify/v1/authenticate", "/spotify/v1/register").permitAll()
                 .anyRequest().authenticated()
                 //don't use sessions cause we use jwt
                 .and().sessionManagement()
@@ -46,6 +50,14 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(customerService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
--- src/main/java/com/bartdebont/spotifyclone/service/CustomerService.java
+++ src/main/java/com/bartdebont/spotifyclone/service/CustomerService.java
@@ -1,17 +1,25 @@
 package com.bartdebont.spotifyclone.service;
 
+import com.bartdebont.spotifyclone.exception.EmailExistsException;
+import com.bartdebont.spotifyclone.model.Customer;
+import com.bartdebont.spotifyclone.model.request.RegisterRequest;
 import com.bartdebont.spotifyclone.repository.CustomerRepository;
+import org.apache.commons.validator.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class CustomerService implements UserDetailsService {
 
     private final CustomerRepository customerRepository;
-//    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     public CustomerService(CustomerRepository customerRepository) {
@@ -19,8 +27,44 @@ public class CustomerService implements UserDetailsService {
     }
 
 
+    public Customer registerNewCustomer(RegisterRequest registerRequest) throws Exception {
+        if (!isValidEmail(registerRequest.getEmail())){
+            throw new Exception("Email is not valid");
+        }
+        if (emailExists(registerRequest.getEmail())) {
+            throw new EmailExistsException(
+                    "There is an account with that email adress:" + registerRequest.getEmail()
+            );
+        }
+        Customer customer = new Customer(
+                registerRequest.getUsername(),
+                registerRequest.getEmail(),
+                passwordEncoder.encode(registerRequest.getPassword()),
+                null,
+                new ArrayList<>());
+        return customerRepository.save(customer);
+    }
+
+    private boolean isValidEmail(String email) {
+//        return EmailValidator.getInstance().isValid(email);
+        return true;
+    }
+
+    private boolean emailExists(String email) {
+        if (customerRepository.findByEmail(email) == null) return false;
+        else return true;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public Customer loadUserByUsername(String s) throws UsernameNotFoundException {
+        try {
+            return getUserByName(s);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private Customer getUserByName(String s) {
+        return customerRepository.findByUsername(s);
     }
 }
